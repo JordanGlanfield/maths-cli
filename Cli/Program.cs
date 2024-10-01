c476cb3ea1af817e750dea6e75f5d45c72fe2017
jordan <j.glanfield@hotmail.co.uk>
2024-10-01 01:00:45 +0100

    Initial commit, add simple command validity check

---
+++ Cli/Program.cs
@@ -0,0 +1,21 @@
+﻿List<string> commands = ["greet", "echo", "sum"];
+
+Console.WriteLine("[{0}]", string.Join(", ", args));
+
+if (args.Length <= 0)
+{
+    Console.WriteLine("Please provide a command");
+    return 0;
+}
+
+var command = args[0];
+
+if (commands.IndexOf(command) < 0)
+{
+    Console.WriteLine("Unrecognised command");
+    return 0;
+}
+
+Console.WriteLine($"At some point I will implement '{command}', hahah");
+
+return 0;
